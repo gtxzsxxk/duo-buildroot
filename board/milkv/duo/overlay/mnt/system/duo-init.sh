6202f4d5b64b066e9cbf40a977aa288c436b7b5a
Hanyuan Zhao <2524395907@qq.com>
2023-09-26 16:43:48 +0800

    refactor: use devmem to initialize the gpio

--- board/milkv/duo/overlay/mnt/system/duo-init.sh
+++ board/milkv/duo/overlay/mnt/system/duo-init.sh
@@ -5,5 +5,6 @@ devmem 0x0502707c 32 0x111
 devmem 0x03001068 32 0x3
 
 # Set Pin-19(SD0_PWR_EN) to GPIO
-cvi_pinmux -w SD0_PWR_EN/XGPIOA_14 > /dev/null
-
+# we do not include the cvi_pinmux currently
+# cvi_pinmux -w SD0_PWR_EN/XGPIOA_14 > /dev/null
+devmem 0x0300101c 32 0x3
