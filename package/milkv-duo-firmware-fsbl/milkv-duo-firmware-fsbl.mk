db38cf8572a82e75a8763ab438a87d139218b396
Hanyuan Zhao <hanyuan-z@qq.com>
2023-10-17 12:38:37 +0800

    feat: update the mk version

--- package/milkv-duo-firmware-fsbl/milkv-duo-firmware-fsbl.mk
+++ package/milkv-duo-firmware-fsbl/milkv-duo-firmware-fsbl.mk
@@ -3,7 +3,7 @@
 # milkv-duo-firmware-fsbl
 #
 ################################################################################
-MILKV_DUO_FIRMWARE_FSBL_VERSION = 3caa7740245943fb0b44090eed8e6f2a4fb7e010
+MILKV_DUO_FIRMWARE_FSBL_VERSION = 4fab0be6930e4db216bea90d489c4d232b4b3cc3
 MILKV_DUO_FIRMWARE_FSBL_SITE = $(call github,gtxzsxxk,milkv-duo-firmware-fsbl,$(MILKV_DUO_FIRMWARE_FSBL_VERSION))
 MILKV_DUO_FIRMWARE_FSBL_INSTALL_STAGING = YES
 MILKV_DUO_FIRMWARE_FSBL_DEPENDENCIES = host-python3
@@ -20,6 +20,7 @@ define MILKV_DUO_FIRMWARE_FSBL_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/build/cv180x/bl2.bin ${BINARIES_DIR}/bl2.bin
 	$(INSTALL) -D -m 0755 $(@D)/plat/cv180x/chip_conf.bin ${BINARIES_DIR}/chip_conf.bin
 	$(INSTALL) -D -m 0755 $(@D)/plat/cv180x/fiptool.py ${BINARIES_DIR}/fiptool.py
+	$(INSTALL) -D -m 0644 $(@D)/plat/cv180x/multi.its ${BINARIES_DIR}/multi.its
 	$(INSTALL) -D -m 0755 $(@D)/test/cv181x/ddr_param.bin ${BINARIES_DIR}/ddr_param.bin
 endef
 
