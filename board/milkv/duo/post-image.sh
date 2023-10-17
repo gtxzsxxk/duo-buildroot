dc1c18c4b1a5fde5a911aa6948289f0b79b109f6
Hanyuan Zhao <hanyuan-z@qq.com>
2023-10-17 15:39:12 +0800

    fix: the paths' mismatching problems

--- board/milkv/duo/post-image.sh
+++ board/milkv/duo/post-image.sh
@@ -7,19 +7,22 @@
 #              then pack everything to an image file.
 ###########################################################
 
-./fiptool.py genfip fip.bin \
+${BINARIES_DIR}/fiptool.py genfip ${BINARIES_DIR}/fip.bin \
     --MONITOR_RUNADDR=0x80000000 \
-    --CHIP_CONF=chip_conf.bin \
+    --CHIP_CONF=${BINARIES_DIR}/chip_conf.bin \
     --NOR_INFO=FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF \
     --NAND_INFO=00000000 \
-    --BL2=bl2.bin \
+    --BL2=${BINARIES_DIR}/bl2.bin \
     --BLCP_IMG_RUNADDR=0x05200200 \
     --BLCP_PARAM_LOADADDR=0 \
-    --DDR_PARAM=ddr_param.bin \
-    --MONITOR=fw_dynamic.bin \
-    --LOADER_2ND=u-boot.bin
+    --DDR_PARAM=${BINARIES_DIR}/ddr_param.bin \
+    --MONITOR=${BINARIES_DIR}/fw_dynamic.bin \
+    --LOADER_2ND=${BINARIES_DIR}/u-boot.bin \
+    > ${BINARIES_DIR}/fip.log 2>&1
 
+cp ${BINARIES_DIR}/u-boot.dtb ${BINARIES_DIR}/cv1800b_milkv_duo_sd.dtb
 lzma -fk ${BINARIES_DIR}/Image
 mkimage -f ${BINARIES_DIR}/multi.its ${BINARIES_DIR}/boot.sd
-support/scripts/genimage.sh -c ${BOARD_DIR}/genimage.cfg
-gzip -fk ${BINARIES_DIR}/milkv-duo_sdcard.img
+echo "> boot.sd generated!"
+support/scripts/genimage.sh -c $(pwd)/board/milkv/duo/genimage.cfg
+gzip -fk ${BINARIES_DIR}/sdcard.img
