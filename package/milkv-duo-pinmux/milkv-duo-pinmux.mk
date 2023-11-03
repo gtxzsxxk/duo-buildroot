cffb2870f942632b61a4ffdb02fe439f4b7c9e42
shodan8192 <23244044+shodan8192@users.noreply.github.com>
2023-11-03 14:12:30 +0100

    feat: add milkv-duo-pinmux

---
+++ package/milkv-duo-pinmux/milkv-duo-pinmux.mk
@@ -0,0 +1,13 @@
+MILKV_DUO_PINMUX_VERSION = c0e32327aaf13490437f485a3fb04bbb4c60f00f
+MILKV_DUO_PINMUX_SITE = $(call github,shodan8192,milkv-duo-pinmux,$(MILKV_DUO_PINMUX_VERSION))
+
+define MILKV_DUO_PINMUX_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) \
+		-I $(@D)/include $(@D)/src/*.c -o $(@D)/duo-pinmux
+endef
+
+define MILKV_DUO_PINMUX_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/duo-pinmux $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(generic-package))
