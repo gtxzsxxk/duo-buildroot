e795b23af0a7dd4b21358638aba17813e956c9c4
shodan8192 <23244044+shodan8192@users.noreply.github.com>
2023-11-06 12:15:49 +0100

    refactor: move usb scripts to /opt/milkv-duo

--- board/milkv/duo/overlay/opt/milkv-duo/usb-mass-storage.sh
+++ board/milkv/duo/overlay/opt/milkv-duo/usb-mass-storage.sh
@@ -3,8 +3,8 @@
 CONFIG=/etc/milkv-duo.conf
 source ${CONFIG}
 
-/etc/uhubon.sh device >> /tmp/usb.log 2>&1
-/etc/run_usb.sh probe msc ${DUO_USB_MASS_STORAGE_ROOT} >> /tmp/usb.log 2>&1
-/etc/run_usb.sh start >> /tmp/usb.log 2>&1
+/opt/milkv-duo/uhubon.sh device >> /tmp/usb.log 2>&1
+/opt/milkv-duo/run_usb.sh probe msc ${DUO_USB_MASS_STORAGE_ROOT} >> /tmp/usb.log 2>&1
+/opt/milkv-duo/run_usb.sh start >> /tmp/usb.log 2>&1
 
 [ $? = 0 ] && echo "Mass Storage started successfully" || echo "Fail to start Mass Storage"
